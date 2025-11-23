56b87d3dc371b8fef5041f3da228c2a1a18d1712
Adithi Ankam <156453527+adithi15@users.noreply.github.com>
2025-11-24 00:21:18 +0530

    Restore middleware implementation in middleware.tsx
    Restored middleware functionality and removed commented-out code.

--- src/middleware.tsx
+++ src/middleware.tsx
@@ -1,3 +1,37 @@
+// import { NextRequest, NextResponse } from "next/server";
+// import jwt from "jsonwebtoken";
+// import { jwtVerify } from "jose";
+
+// export const config = {
+//     runtime: "nodejs",
+//     matcher: ["/dashboard", "/"],
+// };
+
+// export const middleware = async (req: NextRequest) => {
+//     const cookies = req.cookies;
+//     const token = cookies.get("token");
+
+    
+//     if (!token) {
+//         return NextResponse.redirect(new URL("/login", req.url));
+//     }
+
+//     if(req.nextUrl.pathname === "/"){ 
+//         return NextResponse.redirect(new URL("/dashboard", req.url));
+//     }
+
+//     try {
+//         const secret = new TextEncoder().encode(process.env.JWT_SECRET);
+//         let userdata = await jwtVerify(token.value, secret)
+//     } catch (error) {
+//         return NextResponse.redirect(new URL("/login", req.url));
+//     }
+
+
+//     return NextResponse.next();
+// };
+
+
 import { NextRequest, NextResponse } from "next/server";
 import jwt from "jsonwebtoken";
 import { jwtVerify } from "jose";
@@ -8,26 +42,6 @@ export const config = {
 };
 
 export const middleware = async (req: NextRequest) => {
-    const cookies = req.cookies;
-    const token = cookies.get("token");
-
-    
-    if (!token) {
-        return NextResponse.redirect(new URL("/login", req.url));
-    }
-
-    if(req.nextUrl.pathname === "/"){ 
-        return NextResponse.redirect(new URL("/dashboard", req.url));
-    }
-
-    try {
-        const secret = new TextEncoder().encode(process.env.JWT_SECRET);
-        let userdata = await jwtVerify(token.value, secret)
-    } catch (error) {
-        return NextResponse.redirect(new URL("/login", req.url));
-    }
-
 
     return NextResponse.next();
 };
-
